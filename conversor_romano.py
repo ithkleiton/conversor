2858e1b51a430a0c7babd29ed963259b54aa7533
ithkleiton <ithkleiton@hotmail.com>
2023-11-18 10:54:09 -0400

    Update conversores.py

--- conversor_romano.py
+++ conversor_romano.py
@@ -16,3 +16,24 @@ def decimal_para_romano(numero):
 
 print(decimal_para_romano(999))
 
+# abaixo será o codigo de conversão de romano para decimal.
+
+def romano_para_decimal(romano):
+    romano_valores = {'I': 1, 'IV' : 4, 'V': 5 ,'IX': 9, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    resultado = 0
+    anterior = 0
+
+    for letra in romano:
+        valor = romano_valores[letra]
+        resultado += valor
+
+        if valor > anterior:
+            resultado -= 2 * anterior
+        anterior = valor
+
+    return resultado
+
+numero_romano = "MCMXCVIII"
+decimal = romano_para_decimal(numero_romano)
+print(f"O número romano {numero_romano} em decimal é: {decimal}")
